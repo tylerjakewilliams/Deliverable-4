63d8d8402dcf35ae4902be50467ff137a704f2d6
tylerjakewilliams <122945870+tylerjakewilliams@users.noreply.github.com>
2023-02-09 16:36:08 -0500

    Add files via upload

---
+++ Deliverable(4)/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Deliverable_4_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("These are the first 25 numbers of the Fibonacci sequence:");
+            int[] fibonacci = new int[25];
+
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+
+            for (int i = 0; i < 25; i++)
+            {
+                if (i >= 2)
+                {
+                    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+                }
+            }
+
+            foreach (int number in fibonacci)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
